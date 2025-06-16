433daf2f78574ae6119ea07952a86ac2e0e863b4
nardila23 <85463821+nardila23@users.noreply.github.com>
2025-06-16 13:52:12 -0500

    Edited Java logo for about me, changed content of description, and changed image favicon

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,8 +1,7 @@
 import "./AboutContentStyles.css"
 import Vertx from "../assets/Vertx.png"
 // import SpringBoot from "../assets/SpringBoot.png"
-import Java from "../assets/Java.png"
-
+import Java from "../assets/JavaWhiteBackground.jpg"
 import React from 'react'
 import { Link } from "react-router-dom";
 
@@ -11,7 +10,7 @@ const AboutContent = () => {
     <div className="about">
       <div className="left">
         <h1>Who Am I?</h1>
-        <p>I am a software engineer. I mainly have worked in Java, working in the web frameworks of VertX and SpringBoot. However, my interests have taken me into other spaces like ReactJS, Go, and more.</p>
+        <p>I am a software engineer interested in the intersection of Mathematics and Computer Science. With experience in distributed computing research, API development, and large scale applications, I work as an engineer at American Express. My work has been in the realm of Java, using the frameworks of SpringBoot and VertX. Furthermore, my interests have taken me into other spaces like ReactJS, Go, and more.</p>
         <Link to="/contact">
           <button className="btn">Contact</button>
         </Link>
